52511cda3bb9c21fcd8ddcc9c4f1c243e9d45ff7
vardhan <vardhan5a7@gmail.com>
2021-12-09 11:56:57 +0530

    Assignment2scala

--- src/test/scala/arrays/Question3Test.scala
+++ src/test/scala/arrays/Question3Test.scala
@@ -6,7 +6,7 @@ class Question3Test extends AnyFunSuite {
 
   val question3 = new Question3
 
-  test(testName = "Find the maximum products in given array"){
+  test(testName = "Find the maximum difference in given array"){
     assert(question3.findMaximumDifference(Array(8, 3, 1, 7, 9, 5, 11, 3, 5)) == 10)
     assert(question3.findMaximumDifference(Array(23,42,5,3,21,7,12,4,78)) == 75)
   }
